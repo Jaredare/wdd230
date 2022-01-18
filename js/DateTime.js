3921651906b72896d63e63319ac31d8846c54e6a
Jaredare <drakelongtoot@gmail.com>
2022-01-18 10:57:42 -0600

    Switched from inline js for last modified to ext

--- js/DateTime.js
+++ js/DateTime.js
@@ -1,31 +1,34 @@
-let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-]
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+document.getElementById("modify").innerHTML = document.lastModified
 
-let d = new Date();
-let dayName = daynames[d.getDay()];
-let monthName = months[d.getMonth()];
-let year = d.getFullYear();
-let fulldate = `${dayName}, ${monthName} ${d.getDate}, ${year}. - ${d.getTime}`
 
-document.getElementById("currentdate").textContent = fulldate
+// let daynames = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+// ]
+// let months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December"
+// ]
+
+// let d = new Date();
+// let dayName = daynames[d.getDay()];
+// let monthName = months[d.getMonth()];
+// let year = d.getFullYear();
+// let fulldate = `${dayName}, ${monthName} ${d.getDate}, ${year}. - ${d.getTime}`
+
+// document.getElementById("currentdate").textContent = fulldate
